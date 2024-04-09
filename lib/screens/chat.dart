b82d82c0db39e5628c1eb2bac9180f8354200d81
Ike Odira <odiraike8@gmail.com>
2024-04-09 02:21:22 -0400

    finished chat app

---
+++ lib/screens/chat.dart
@@ -0,0 +1,62 @@
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission(sound: true);
+
+    //get token and send message to specific devices
+    // final token = await fcm.getToken();
+    // print(token); // you can send this token (via HTTP or the firestore SDK) to a backend
+
+    //
+    fcm.subscribeToTopic("chat");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "ChatApp",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(Icons.exit_to_app, color: Theme.of(context).colorScheme.primary),
+          ),
+        ],
+      ),
+      body: const Column(
+        children: [
+          Expanded(
+            child: ChatMessages(),
+          ),
+          NewMessage(),
+        ],
+      ),
+    );
+  }
+}
